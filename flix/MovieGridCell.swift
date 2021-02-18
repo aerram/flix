3c6eaf2cfacaa823257934c8550fd523e46184b6
anoushkaerram <anoushkaerram@gmail.com>
2021-02-17 21:26:32 -0800

    first commit

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Anoushka Erram on 2/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
